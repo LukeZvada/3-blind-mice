991f22ae2f464e7b84e45cf8ee9c65d7a16b2444
LukeZvada <zvadamusic@gmail.com>
2020-08-21 12:51:47 -0700

    fixed function name

--- scripts/Employees/employeeDataProvider.js
+++ scripts/Employees/employeeDataProvider.js
@@ -4,7 +4,7 @@ export const useEmployees = () => {
     return employees.slice()
 }
 
-export const getCriminals = () => { 
+export const getEmployees = () => { 
     return fetch("http://localhost:8088/notes")
         .then(response => response.json())
         .then(
