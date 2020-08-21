e2a6806c01654a4253cadc406b270d85f4453bf9
LukeZvada <zvadamusic@gmail.com>
2020-08-21 13:03:20 -0700

    added employeeHTMLConverter

---
+++ scripts/Employees/employee.js
@@ -0,0 +1,9 @@
+export const employeeHTMLConverter = (employeeObject) => {
+    return ` 
+        <section class="employee">
+            <div class="name"> </div>
+            <div class="age"> </div>
+            <div class="computerModel"> </div> 
+        </section>
+    `
+}
