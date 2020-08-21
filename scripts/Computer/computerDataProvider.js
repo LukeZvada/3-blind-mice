b394dc2bbdca96003e7ddd0ce2822677288bd4e3
LukeZvada <zvadamusic@gmail.com>
2020-08-21 12:55:00 -0700

    added computer fetch call

--- scripts/Computer/computerDataProvider.js
+++ scripts/Computer/computerDataProvider.js
@@ -1,5 +1,18 @@
 let computers = []
 
-const getComputers = () => {
+const useComputers = () => {
     return computers.slice()
 }
+
+
+export const getComputers = () => { 
+    return fetch("http://localhost:8088/database")
+        .then(response => response.json())
+        .then(
+            parsedComputers => {
+                criminals = parsedComputers
+            }
+        )
+}
+
+
