7ff508fa2f63a1170a7f3689ecb0d08c28b1c1ba
LukeZvada <zvadamusic@gmail.com>
2020-08-21 12:51:18 -0700

    using and getting Employees

--- scripts/Employees/employeeDataProvider.js
+++ scripts/Employees/employeeDataProvider.js
@@ -1,2 +1,15 @@
 let employees = []
 
+export const useEmployees = () => {
+    return employees.slice()
+}
+
+export const getCriminals = () => { 
+    return fetch("http://localhost:8088/notes")
+        .then(response => response.json())
+        .then(
+            parsedEmployees => {
+                criminals = parsedEmployees
+            }
+        )
+}
